8cfd1cf3c991a5055d2817b54da6759600252bd2
Ricardo Ogliari <ricardo@stillrocker.com>
2013-09-22 09:52:08 -0300

    Initial Commit

---
+++ AHWIntentRota/src/com/example/ahw/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ahw;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void rota(View v){
+		Intent intent = new Intent(
+				android.content.Intent.ACTION_VIEW, Uri
+						.parse("google.navigation:q="
+								+ "avenida santa marina, 381, são paulo"));
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
